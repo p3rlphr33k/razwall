9a12ce49ab2a4b7b264d653ee0687e962ddede3c
Bryan King <bryan@ndkings.com>
2025-03-13 08:56:06 -0500

    updates and fixes
    rebuild razwall package wit the following:
    dhcpd service script
    rc.local containing dhcpd startup and web socket service startup
    updated doinst.sh to generate SSL certificate and set permissions

--- pkgbuild/install/doinst.sh
+++ pkgbuild/install/doinst.sh
@@ -1,1 +1,18 @@
- echo "RazWall package has been installed."
+echo "Setting permissions.."
+
+chmod 0777 /razwall/web/cgi-bin/*.pl
+chmod 0777 /razwall/web/cgi-bin/*.cgi
+chmod 0777 /razwall/web/cgi-bin/*.pm
+chmod 0777 /razwall/web/cgi-bin/setup/step1/*.pl
+chmod 0777 /razwall/web/cgi-bin/setup/step1/*.cgi
+chmod 0777 /razwall/web/cgi-bin/setup/step2/*.pl
+chmod 0777 /razwall/web/cgi-bin/setup/step2/*.cgi
+chmod 0777 /razwall/web/cgi-bin/*.sh
+chmod +x /razwall/web/cgi-bin/*.sh
+chmod +x /etc/rc.d/rc.dhcpd
+
+echo "Generating HTTP SSL Certificate.."
+
+openssl req -x509 -nodes -days 356 -newkey rsa:4096 -keyout /razwall/web/certs/razwall.key -out /razwall/web/certs/razwall.crt -subj '/C=US/ST=North Dakota/L=Reynolds/O=Supervene LLC/CN=localhost'
+
+echo "RazWall package has been installed."
